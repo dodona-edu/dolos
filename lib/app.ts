762acaf6d3240da4302d3c45308923351f0655bc
baconandchips <16443280+baconandchips@users.noreply.github.com>
2019-07-04 12:02:51 +0200

    improvements on formatting

--- lib/app.ts
+++ lib/app.ts
@@ -1,4 +1,22 @@
 import RollingHash from "./rollingHash";
+import Winnowing from "./winnowing";
+
+const k = 20;
+const hash = new RollingHash(k);
+let solnArr: number[];
+
+process.stdin.on("data", data => {
+    solnArr = new Array(data.length);
+    let s = "";
+    data.forEach((b: number) => {
+        s += hash.nextHash(b) + "\n";
+    });
+    console.log(s);
+});
+
+/*
+
+import RollingHash from "./rollingHash";
 
 const k = 20;
 const hash = new RollingHash(k);
@@ -10,3 +28,5 @@ process.stdin.on("data", data => {
     });
     console.log(s);
 });
+
+*/
--- lib/winnowing.ts
+++ lib/winnowing.ts
@@ -1,29 +1,30 @@
 import RollingHash from "./rollingHash";
 
 export default class Winnowing {
-    private readonly w: number = 25;
+    private readonly k: number = 20;
     private hash: RollingHash; 
     private str: string;
     private arr: number[];
     private solnArr: number[];
 
     
-    constructor(hash: RollingHash, str: string, w?: number) {
-        this.w = w ? w: this.w;
+    constructor(hash: RollingHash, str: string, k?: number) {
+        this.k = k ? k: this.k;
         this.hash = hash;
         this.str = str;
-        this.arr = new Array(w);
-        this.solnArr = new Array(str.length);
+        this.arr = new Array(k);
+        this.solnArr = new Array(this.str.length);
     }
 
     /*
      * Finds our smallest hash value in our window
+     
      * @param void There are no parameters for our function, all are in class
      */
 
      public winnow(): void {
          let maxVal = Math.pow(2, 31) - 1;  // NOT THE SMALLEST VALUE AVAILABLE
-         for(let j: number = 0; j < this.w; ++j) {
+         for(let j: number = 0; j < this.k; ++j) {
              this.arr[j] =  maxVal;
          }
          let r: number = 0;
@@ -31,24 +32,24 @@ export default class Winnowing {
          let currentIteration: number = 0;
 
          while (1) {
-             r = (r + 1 % this.w);
+             r = (r + 1 % this.k);
              this.arr[r] =  this.hash.nextHash(r);
              if (0) { 
                  // terminating condition; I suspect that we need one?
              }
              if (min === r) {
-                 for (let i = (r - 1) % this.w; i !== r; i = (i - 1 + this.w) % this.w) {
+                 for (let i = (r - 1) % this.k; i !== r; i = (i - 1 + this.k) % this.k) {
                      if (this.arr[i] < this.arr[min]) {
                         min = i;
                      }
                  }
-                 return this.record(this.arr[min], this.globalPos(min, r, this.w, currentIteration));
+                 return this.record(this.arr[min], this.globalPos(min, r, this.k, currentIteration));
              }
 
              else {
                  if (this.arr[r] < this.arr[min]) {
                      min = r;
-                     return this.record(this.arr[min], this.globalPos(min, r, this.w, currentIteration));
+                     return this.record(this.arr[min], this.globalPos(min, r, this.k, currentIteration));
                  }
              }
              currentIteration++;
@@ -72,9 +73,12 @@ export default class Winnowing {
        * Create a global position for our hash
        * 
        * @param min the minimum hash value on here
+       * @param r the rightmost element in circular array
+       * @param k the size of the word
+       * @param currentIternation is total size of array
        */
 
-      public globalPos(min: number, r: number, w: number, currentIteration: number) {
-          return (currentIteration + (min - r + w - 1) % w);
+      public globalPos(min: number, r: number, k: number, currentIteration: number) {
+          return (currentIteration + (min - r + k - 1) % k);
       }
 }
