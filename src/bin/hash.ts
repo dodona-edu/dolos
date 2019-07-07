8c469d2e32148fef62164ffd689e809cb427cd41
Bart Mesuere <Bart.Mesuere@UGent.be>
2019-07-07 20:44:24 +0200

    add additional run scripts

---
+++ src/bin/hash.ts
@@ -0,0 +1,12 @@
+import {RollingHash} from "../lib/rollingHash";
+
+const k = 20;
+const hash = new RollingHash(k);
+
+process.stdin.on("data", data => {
+  let s = "";
+  data.forEach((b: number) => {
+    s += hash.nextHash(b) + "\n";
+  });
+  console.log(s);
+});
