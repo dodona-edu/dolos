1275d9dd626927f876340e69c235a839528499cd
Bart Mesuere <Bart.Mesuere@UGent.be>
2019-07-07 17:32:13 +0200

    fix linting error

--- src/lib/__tests__/rollingHash.test.ts
+++ src/lib/__tests__/rollingHash.test.ts
@@ -4,9 +4,9 @@ test("hash is independant of prefix", () => {
   const hash1 = new RollingHash(3);
   const hash2 = new RollingHash(3);
   // first add a to hash1, then add bcd to both
-  for (const c of "abc") hash1.nextHash(c.charCodeAt(0));
+  for (const c of "abc") { hash1.nextHash(c.charCodeAt(0)); }
 
-  for (const c of "bc") hash2.nextHash(c.charCodeAt(0));
+  for (const c of "bc") { hash2.nextHash(c.charCodeAt(0)); }
 
   const h1 = hash1.nextHash("d".charCodeAt(0));
   const h2 = hash2.nextHash("d".charCodeAt(0));
