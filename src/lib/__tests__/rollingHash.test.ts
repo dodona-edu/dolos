7e1c5cd55bbfddd35f343914f9f8887ae5e8db5d
Bart Mesuere <Bart.Mesuere@UGent.be>
2019-07-07 20:30:53 +0200

    add more tests

--- src/lib/__tests__/rollingHash.test.ts
+++ src/lib/__tests__/rollingHash.test.ts
@@ -5,10 +5,15 @@ test("hash is independant of prefix", () => {
   const hash2 = new RollingHash(3);
   // first add a to hash1, then add bcd to both
   for (const c of "abc") { hash1.nextHash(c.charCodeAt(0)); }
-
   for (const c of "bc") { hash2.nextHash(c.charCodeAt(0)); }
 
   const h1 = hash1.nextHash("d".charCodeAt(0));
   const h2 = hash2.nextHash("d".charCodeAt(0));
   expect(h1).toBe(h2);
+});
+
+test("hash value is consistent", () => {
+  const hash =  new RollingHash(3);
+  for (const c of "abc") { hash.nextHash(c.charCodeAt(0)); }
+  expect(hash.nextHash("d".charCodeAt(0))).toBe(10093531);
 });
