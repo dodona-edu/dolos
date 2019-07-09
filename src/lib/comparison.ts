eeb226bb9656bbf9b5b0a2f4442b3b419221f4ee
Bart Mesuere <Bart.Mesuere@UGent.be>
2019-07-09 21:32:43 +0200

    create index based on work of @toonijn and @baconandchips

---
+++ src/lib/comparison.ts
@@ -0,0 +1,57 @@
+import { HashFilter } from "./hashFilter";
+import { Tokenizer } from "./tokenizer";
+import { WinnowFilter } from "./winnowFilter";
+
+type Matches = Map<string, Array<[number, number]>>;
+
+export class Comparison {
+  private readonly defaultK: number = 20;
+  private readonly defaultW: number = 40;
+  private readonly index: Map<number, Array<[string, number]>> = new Map();
+  private readonly tokenizer: Tokenizer;
+  private readonly hashFilter: HashFilter;
+
+  constructor(tokenizer: Tokenizer, hashFilter?: HashFilter) {
+    this.tokenizer = tokenizer;
+    this.hashFilter = hashFilter ? hashFilter : new WinnowFilter(this.defaultK, this.defaultW);
+  }
+
+  public addFiles(files: string[]): Promise<void[]> {
+    return Promise.all(files.map(this.addFile));
+  }
+
+  public async addFile(file: string): Promise<void> {
+    const [ast, mapping] = await this.tokenizer.tokenizeFileWithMapping(file);
+    for await (const [hash, position] of this.hashFilter.hashesFromString(ast)) {
+      // hash and the corresponding line number
+      const match: [string, number] = [file, mapping[position]];
+      const matches = this.index.get(hash);
+      if (matches) {
+        matches.push(match);
+      } else {
+        this.index.set(hash, [match]);
+      }
+    }
+  }
+
+  public async compare(file: string, hashFilter = this.hashFilter): Promise<Matches> {
+    // mapping file names to line number pairs (other file, this file)
+    const matchingFiles: Matches = new Map();
+    const [ast, mapping] = await this.tokenizer.tokenizeFileWithMapping(file);
+    for await (const [hash, position] of hashFilter.hashesFromString(ast)) {
+      const matches = this.index.get(hash);
+      if (matches) {
+        for (const [fileName, lineNumber] of matches) {
+          const match: [number, number] = [lineNumber, mapping[position]];
+          const lines = matchingFiles.get(fileName);
+          if (lines) {
+            lines.push(match);
+          } else {
+            matchingFiles.set(fileName, [match]);
+          }
+        }
+      }
+    }
+    return matchingFiles;
+  }
+}
