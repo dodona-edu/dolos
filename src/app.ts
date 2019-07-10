d4f7863e7617ac4f0e64c56bffc9bac7808d88af
Bart Mesuere <Bart.Mesuere@UGent.be>
2019-07-10 18:09:07 +0200

    compare multiple files, add docs

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,6 @@ import { Tokenizer } from "./lib/tokenizer";
   const tokenizer = new Tokenizer("javascript");
   const comparison = new Comparison(tokenizer);
   await comparison.addFile("samples/js/sample.js");
-  const result = await comparison.compare("samples/js/copied_function.js");
+  const result = await comparison.compareFile("samples/js/copied_function.js");
   console.log(result);
 })();
