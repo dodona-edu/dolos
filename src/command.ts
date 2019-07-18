0ebc95cf7930df9d46754681e3fda7c225ea2450
baconandchips <16443280+baconandchips@users.noreply.github.com>
2019-07-18 17:24:22 +0200

    CLI with optional parameters
    can compare 2 different files; for now it compares the two sample files on javascript by default

--- src/command.ts
+++ src/command.ts
@@ -1,6 +1,8 @@
+const commander = require('commander');
+
 import { Comparison } from "./lib/comparison";
 import { CodeTokenizer } from "./lib/codeTokenizer";
-import commander from 'commander';
+
 let packageJson = require('../package.json');
 
 const program = new commander.Command();
@@ -11,29 +13,17 @@ program
     .description('Plagarism detection for programming exercises');
 
 program
-    .command('compare <language> <path1> <path2>')
+    .command('compare [language] [path1] [path2]')
     .alias('c')
     .description('Compare source codes')
-    .action(async (language:string, path1:string, path2:string) => {
-        // copied from app.ts
+    .action(async (language:string, path1:string, path2:string):Promise<void> => {
+        // from app.ts
+        console.log("Entering compare");
         const tokenizer = new CodeTokenizer(language ? language: "javascript");
         const comparison = new Comparison(tokenizer);
         await comparison.addFiles([path1 ? path1: "samples/js/samples.js", path2 ? path2: "samples/js/sample.js"]);
         const result = await comparison.compareFile("samples/js/copied_function.js");
         console.log(result);
     });
-
-/*
-using yarn test is better
-
-program
-    .command('test')
-    .alias('t')
-    .description('Test Dolos for accuracy')
-    .action( () => {
-        // connect to testing functions over here
-
-    });
-*/
 
 program.parse(process.argv);
