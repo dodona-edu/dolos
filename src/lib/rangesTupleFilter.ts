c5e0e3f6e40fbca6fd9ba36d6f7600b995a22002
Arne Jacobs <arne.jacobs@ugent.be>
2019-08-12 16:24:45 +0200

    implemented more filter methods

--- src/lib/rangesTupleFilter.ts
+++ src/lib/rangesTupleFilter.ts
@@ -1,3 +1,4 @@
+import { stringLiteral } from "@babel/types";
 import { Matches } from "./comparison";
 import { RangesTuple } from "./summary";
 
@@ -28,14 +29,16 @@ export class RangesTupleFilter {
           this.minimumMinimumLines,
     );
   }
-
-  // TODO
-  public filterByMaximumPassagePercentage(matchesPerFile: Map<string, Matches<number>>, groups?: number): Map<string, Matches<number>> {
+  // TODO test
+  public filterByMaximumPassagePercentage(
+    matchesPerFile: Map<string, Matches<number>>,
+    groupAmount: number,
+  ): Map<string, Matches<number>> {
     const lineCountPerFile: Map<string, Map<number, number>> = new Map();
     matchesPerFile.forEach((matches, matchingFileName) => {
       matches.forEach((matchingLinesArray, matchedFileName) => {
-        let matchesLinesCount: Map<number, number> ;
-        if(lineCountPerFile.has(matchingFileName)) {
+        let matchesLinesCount: Map<number, number>;
+        if (lineCountPerFile.has(matchingFileName)) {
           matchesLinesCount = lineCountPerFile.get(matchingFileName) as Map<number, number>;
         } else {
           matchesLinesCount = new Map();
@@ -43,25 +46,60 @@ export class RangesTupleFilter {
         }
 
         let matchedLinesCount: Map<number, number>;
-        if(lineCountPerFile.has(matchedFileName)) {
+        if (lineCountPerFile.has(matchedFileName)) {
           matchedLinesCount = lineCountPerFile.get(matchedFileName) as Map<number, number>;
         } else {
           matchedLinesCount = new Map();
           lineCountPerFile.set(matchedFileName, matchedLinesCount);
         }
 
-        matchingLinesArray.forEach((matchingLines) => {
-          matchesLinesCount.set(matchingLines[0], (matchesLinesCount.get(matchingLines[0]) || 0) + 1);
-          matchedLinesCount.set(matchingLines[1], (matchedLinesCount.get(matchingLines[1]) || 0) + 1);
-        })
-
-
+        matchingLinesArray.forEach(matchingLines => {
+          matchesLinesCount.set(
+            matchingLines[0],
+            (matchesLinesCount.get(matchingLines[0]) || 0) + 1,
+          );
+          matchedLinesCount.set(
+            matchingLines[1],
+            (matchedLinesCount.get(matchingLines[1]) || 0) + 1,
+          );
+        });
       });
     });
-    return matchesPerFile;
+    const returnMap: Map<string, Matches<number>> = new Map();
+
+    matchesPerFile.forEach((matches, matchingFileName) => {
+      const filteredMatchingFileName: Map<string, Array<[number, number]>> = new Map();
+      const matchingFileNameLinesCount: Map<number, number> = lineCountPerFile.get(
+        matchingFileName,
+      ) as Map<number, number>;
+      matches.forEach((matchingLinesArray, matchedFileName) => {
+        const matchedFileNameLinesCount: Map<number, number> = lineCountPerFile.get(
+          matchedFileName,
+        ) as Map<number, number>;
+        const filteredMatchingLinesArray = this.unique(matchingLinesArray).filter(matchingLines => {
+          const matchingLineCount: number = matchingFileNameLinesCount.get(
+            matchingLines[0],
+          ) as number;
+          const matchedLineCount: number = matchedFileNameLinesCount.get(
+            matchingLines[1],
+          ) as number;
+          return (
+            matchingLineCount / groupAmount <= this.maximumPassagePercentage &&
+            matchedLineCount / groupAmount <= this.maximumPassagePercentage
+          );
+        });
+        if (filteredMatchingLinesArray.length > 0) {
+          filteredMatchingFileName.set(matchedFileName, filteredMatchingLinesArray);
+        }
+      });
+      if (filteredMatchingFileName.size > 0) {
+        returnMap.set(matchingFileName, filteredMatchingFileName);
+      }
+    });
+    return returnMap;
   }
 
-  // TODO
+  //TODO test
   public filterByBaseFile(
     matchingLinesArray: Array<[number, number]>,
     baseFileMatches: RangesTuple[],
@@ -69,8 +107,23 @@ export class RangesTupleFilter {
     const matchedFileRangesArray = baseFileMatches.map(rangesTuple => rangesTuple[1]);
     // it is assumed that the order of the tuples withing basefileMatches is: [range corresponding with the basefile, range corresponding with the matched file]
     // the first element in each number tuple will be assumed to belong to the matched file you want to filter
-    return matchingLinesArray.filter(matchingLines =>
-      !matchedFileRangesArray.some(range => range.isNumberWithingBounds(matchingLines[0])),
+    return matchingLinesArray.filter(
+      matchingLines =>
+        !matchedFileRangesArray.some(range => range.isNumberWithingBounds(matchingLines[0])),
     );
+  }
+
+  private contains(list: Array<[number, number]>, item: [number, number]): boolean {
+    return list.some(listItem => listItem[0] === item[0] && listItem[1] === item[1]);
+  }
+
+  private unique(list: Array<[number, number]>): Array<[number, number]> {
+    const returnArray: Array<[number, number]> = new Array();
+    list.forEach(value => {
+      if (!this.contains(returnArray, value)) {
+        returnArray.push(value);
+      }
+    });
+    return returnArray;
   }
 }
