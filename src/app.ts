d0f07342c41e279ce4a83bd64efa1e1e3cf06d45
Arne Jacobs <arne.jacobs@ugent.be>
2019-08-14 14:06:27 +0200

    removed commented out code in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,22 @@
 import { CodeTokenizer } from "./lib/codeTokenizer";
 import { Comparison } from "./lib/comparison";
 import { Summary } from "./lib/summary";
+import { SummaryFilter } from "./lib/summaryFilter";
 
 (async () => {
-  const tokenizer = new CodeTokenizer("python");
+  const tokenizer = new CodeTokenizer("javascript");
   const comparison = new Comparison(tokenizer);
-  // let path = 'samples/python/snowball-numbers/';
 
-  // let files = fs.readdirSync(path);
-  // await comparison.addFiles([path + files[0], path + files[1]]);
 
-  // let arr = [];
-  // for (const key of Array(files.length -3 ).keys()) {
-  //   arr.push(path + files[key + 2]);
-  // }
-  // const result = await comparison.compareFiles(arr);
 
-  await comparison.addFiles(["samples/js/sample.js"]);
-  const result = await comparison.compareFiles([
-    "samples/js/copied_function.js",
+  await comparison.addFiles([
     "samples/js/another_copied_function.js",
   ]);
-  const summary = new Summary(result);
+  const result = await comparison.compareFiles([
+    "samples/js/sample.js",
+    "samples/js/copied_function.js",
+  ]);
+  const summaryFilter = new SummaryFilter(1, 1);
+  const summary = new Summary(result, summaryFilter);
   console.log(summary.toString());
 })();
