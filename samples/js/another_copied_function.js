710ebdd5d86f17efa897ae3a8cdf7dac51b2e6ee
Rien Maertens <rien.maertens@posteo.be>
2019-11-05 14:24:06 +0100

    Fix failing test

--- samples/js/another_copied_function.js
+++ samples/js/another_copied_function.js
@@ -2,6 +2,7 @@ this.combineer = combineer || ((h, v) => (h + v) % 256);
 
 function is_geldig() {
     return (
-        (!this.parent || this.parent.is_geldig()) && this.hash === this.hasher.hash(`${this.index}${this.datum}${this.vorige_hash}`)
+        (!this.parent || this.parent.is_geldig()) &&
+        this.hash === this.hasher.hash(`${this.index}${this.datum}${this.vorige_hash}`)
     );
 }
