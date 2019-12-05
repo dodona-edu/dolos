738a73b70b1d6f83c5126fa8cff14a934b11a53a
Rien Maertens <rien.maertens@posteo.be>
2019-12-05 15:42:44 +0100

    Move tokenizer test

--- src/test/tokenizer.test.ts
+++ src/test/tokenizer.test.ts
@@ -1,9 +1,31 @@
 import test from "ava";
+import * as path from "path";
 import { CodeTokenizer } from "../lib/tokenizers/codeTokenizer";
 
-test("tokenizer creation works for all supported languages", t => {
+test("tokenizer creation works for all listed languages", t => {
   for (const language of CodeTokenizer.supportedLanguages) {
-    const tokenizer = new CodeTokenizer(language);
-    t.false(tokenizer === undefined || tokenizer === null);
+    t.truthy(new CodeTokenizer(language));
   }
+});
+
+test("tokenizer creation throws error for unsupported language", t => {
+  t.throws(() => new CodeTokenizer("some string"));
+});
+
+test("registering a new installed language works", t => {
+  t.is(undefined, CodeTokenizer.registerLanguage("python"));
+});
+
+test("registering a new invalid language throws error", t => {
+  t.throws(() => CodeTokenizer.registerLanguage("some string"));
+});
+
+test("tokenizer with or without location is equal", async t => {
+  const tokenizer = new CodeTokenizer("javascript");
+  const file = path.resolve(`samples/js/sample.js`);
+
+  const [tokenized, mapping] = await tokenizer.tokenizeFileWithMapping(file);
+
+  t.snapshot(tokenized);
+  t.snapshot(mapping);
 });
