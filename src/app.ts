09655e62242b68a7c2fd7ffef3d1f90b63b5e357
Rien Maertens <rien.maertens@posteo.be>
2020-02-04 13:43:02 +0100

    Remove non-existing file

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import { Comparison } from "./lib/comparison";
 (async () => {
   const tokenizer = new CodeTokenizer("javascript");
   const comparison = new Comparison(tokenizer);
-  await comparison.addFiles(["samples/js/samples.js", "samples/js/sample.js"]);
+  await comparison.addFiles(["samples/js/sample.js"]);
   const result = await comparison.compareFile("samples/js/copied_function.js");
   console.log(result);
 })();
