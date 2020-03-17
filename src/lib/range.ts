bf6b44eef20ede3638ce75816848a7a7911ea462
Rien Maertens <rien.maertens@posteo.be>
2020-03-17 17:12:09 +0100

    Create Range class

---
+++ src/lib/range.ts
@@ -0,0 +1,42 @@
+import assert from "assert";
+
+/**
+ * A range of whole numbers starting at `from` (inclusive) and ending at `to`
+ * (Exclusive).
+ */
+export class Range {
+
+  public static merge(one: Range, other: Range): Range {
+    return new Range(
+      Math.min(one.from, other.from),
+      Math.max(one.to, other.to)
+    );
+  }
+
+  constructor(
+    public readonly from: number,
+    public readonly to: number = -1
+  ){
+    if(this.to == -1) {
+      this.to = this.from + 1;
+    }
+    assert(
+      this.from < this.to,
+      "'from' should be smaller than 'to'"
+    );
+  }
+
+  get length(): number {
+    return this.to - this.from - 1;
+  }
+
+  public overlapsWith(other: Range): boolean {
+    if (this.from < other.from) {
+      return this.to > other.from
+    } else if (this.from > other.from) {
+      return other.to > this.from;
+    } else { // 'from' overlaps
+      return true;
+    }
+  }
+}
---
+++ src/test/range.ts
@@ -0,0 +1,33 @@
+import test from "ava";
+import { Range } from "../lib/range";
+
+test("illegal range", t => {
+  t.throws(() => new Range(1337, 42));
+  t.throws(() => new Range(69, 69));
+})
+
+test("range length", t => {
+  t.is(0, new Range(10).length);
+  t.is(1, new Range(10, 12).length);
+  t.is(100, new Range(0, 101).length);
+});
+
+test("range overlap", t => {
+  t.is(true, new Range(1, 5).overlapsWith(new Range(3, 4)));
+  t.is(true, new Range(1, 4).overlapsWith(new Range(3, 4)));
+  t.is(true, new Range(3, 5).overlapsWith(new Range(1, 5)));
+  t.is(true, new Range(3, 5).overlapsWith(new Range(1, 4)));
+  t.is(true, new Range(1).overlapsWith(new Range(1)));
+
+  t.is(false, new Range(1, 2).overlapsWith(new Range(3, 4)));
+  t.is(false, new Range(1, 3).overlapsWith(new Range(3, 4)));
+  t.is(false, new Range(3, 5).overlapsWith(new Range(1, 3)));
+  t.is(false, new Range(4, 5).overlapsWith(new Range(1, 3)));
+});
+
+test("range merge", t => {
+  t.deepEqual(new Range(1, 5), Range.merge(new Range(1, 2), new Range(3, 5)));
+  t.deepEqual(new Range(1, 5), Range.merge(new Range(1, 4), new Range(2, 5)));
+  t.deepEqual(new Range(1, 5), Range.merge(new Range(4, 5), new Range(1, 2)));
+  t.deepEqual(new Range(1, 2), Range.merge(new Range(1, 2), new Range(1, 2)));
+});
