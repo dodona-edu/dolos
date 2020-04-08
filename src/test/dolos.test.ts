7a6d21c7d1f25aa1457551b140e13872a85494c8
Rien Maertens <rien.maertens@posteo.be>
2020-04-08 13:49:54 +0200

    Check for changed order

--- src/test/dolos.test.ts
+++ src/test/dolos.test.ts
@@ -33,11 +33,11 @@ test("equal content should be a full match", async t => {
   t.is(intersection.matches.length, 1);
   const match = intersection.matches[0];
 
-  t.deepEqual(match.leftSelection, new Selection(2, 2, 11, 2));
-  t.deepEqual(match.rightSelection, new Selection(2, 2, 11, 2));
+  t.deepEqual(new Selection(2, 2, 11, 2), match.leftSelection);
+  t.deepEqual(new Selection(2, 2, 11, 2), match.rightSelection);
 
-  t.deepEqual(match.leftKmers, new Range(0, 24));
-  t.deepEqual(match.rightKmers, new Range(0, 24));
+  t.deepEqual(new Range(0, 24), match.leftKmers);
+  t.deepEqual(new Range(0, 24), match.rightKmers);
 
 });
 
@@ -83,12 +83,12 @@ test("renamed variables should be a full match", async t => {
   t.is(intersection.matches.length, 1);
   const match = intersection.matches[0];
 
-  t.deepEqual(match.leftSelection, new Selection(2, 2, 11, 2));
-  t.deepEqual(match.rightSelection, new Selection(2, 2, 11, 2));
+  t.deepEqual(new Selection(2, 2, 11, 2), match.leftSelection);
+  t.deepEqual(new Selection(2, 2, 11, 2), match.rightSelection);
 
-  t.deepEqual(match.leftKmers, new Range(0, 24));
-  t.deepEqual(match.rightKmers, new Range(0, 24));
-})
+  t.deepEqual(new Range(0, 24), match.leftKmers);
+  t.deepEqual(new Range(0, 24), match.rightKmers);
+});
 
 test("changed whitespace and semicolons should be a full match", async t => {
   const dolos = new Dolos();
@@ -135,6 +135,50 @@ test("changed whitespace and semicolons should be a full match", async t => {
   t.is(intersection.matches.length, 1);
   const match = intersection.matches[0];
 
-  t.deepEqual(match.leftKmers, new Range(0, 24));
-  t.deepEqual(match.rightKmers, new Range(0, 24));
-})
+  t.deepEqual(new Range(0, 24), match.leftKmers);
+  t.deepEqual(new Range(0, 24), match.rightKmers);
+});
+
+test("changed order should be a good match", async t => {
+  const dolos = new Dolos();
+  const original = `
+
+  function hello() {
+    return "hello";
+  }
+
+  const world = () => "world";
+
+  function helloWorld() {
+    console.log(hello() + " " + world())
+  }
+  `;
+
+  const changed = `
+
+  function helloWorld() {
+    console.log(hello() + " " + world())
+  }
+
+  function hello() {
+    return "hello";
+  }
+
+  const world = () => "world";
+  `;
+
+  const analysis = await dolos.analyze(
+    [
+      new File("file1", original),
+      new File("file2", changed),
+    ]
+  );
+
+  t.is(analysis.intersections().length, 1);
+  const { intersection, similarity } = analysis.scoredIntersections()[0];
+  t.true(similarity > 0.75);
+
+  // four matches: program declaration, hello(), world() and helloWorld()
+  t.is(4, intersection.matches.length);
+
+});
