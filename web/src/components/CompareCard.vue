e855322eff3693baca88ae5183da0170ca3acfa2
Arne Jacobs <arne.jacobs@ugent.be>
2020-08-03 16:36:57 +0200

    added line numbering

--- web/src/components/CompareCard.vue
+++ web/src/components/CompareCard.vue
@@ -1,49 +1,64 @@
 <template>
-    <v-card :loading="!loaded">
-      <v-card-title>
-        One
-        <v-spacer />
-        Other
-      </v-card-title>
-      <v-container fluid>
-        <v-row v-if="loaded && intersection" justify="center">
-          <v-col sm="6">
-            <pre>
-              <span v-html="codeLeft"></span>
-            </pre>
-          </v-col>
-          <v-col sm="6">
-            <pre>
-                <span v-html="codeRight"></span>
-            </pre>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card>
+  <v-card :loading="!loaded">
+    <v-card-title>
+      One
+      <v-spacer/>
+      Other
+    </v-card-title>
+    <v-container fluid>
+      <v-row v-if="loaded && intersection" justify="center">
+        <v-col sm="6">
+          <pre class="line-numbers language-javascript"><code ref="codeLeft">{{codeLeft}}</code></pre>
+        </v-col>
+        <v-col sm="6">
+          <pre class="line-numbers language-javascript"><code ref="codeRight">{{codeRight}}</code></pre>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
-<style lang="css" src="@/../public/prism.css"></style>
-
 <script lang="ts">
 
 import { Component, Vue, Prop } from "vue-property-decorator";
 import { Intersection } from "@/api/api";
 import Prism from "prismjs";
+import "prismjs/themes/prism.css";
+import "prismjs/components/prism-javascript";
+import "prismjs/plugins/line-numbers/prism-line-numbers.js";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 
-@Component
+  @Component
 export default class Compare extends Vue {
-  @Prop({ default: false }) loaded!: boolean;
-  @Prop() intersection!: Intersection;
+    @Prop({ default: false }) loaded!: boolean;
+    @Prop() intersection!: Intersection;
 
-  get codeRight(): string {
-    return Prism.highlight(this.intersection.rightFile.content, Prism.languages.javascript, "javascript");
-  }
+    get codeRight(): string {
+      return this.intersection.rightFile.content;
+    }
 
-  get codeLeft(): string {
-    return Prism.highlight(this.intersection.leftFile.content, Prism.languages.javascript, "javascript");
-  }
+    get codeLeft(): string {
+      return this.intersection.leftFile.content;
+    }
+
+    mounted(): void {
+      this.highlight();
+    }
+
+    updated(): void {
+      this.highlight();
+    }
+
+    highlight(): void {
+      Prism.highlightElement(this.$refs.codeLeft as Element, false);
+      Prism.highlightElement(this.$refs.codeRight as Element, false);
+    }
 }
 </script>
 
-<style scoped>
+<style>
+  .reveal pre.line-numbers > code {
+    overflow: visible;
+    padding: 0;
+  }
 
 </style>
