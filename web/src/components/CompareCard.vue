e2f6b41f7b70017e03eb17596a6bc83f244187cf
Arne Jacobs <arne.jacobs@ugent.be>
2020-08-10 11:45:22 +0200

    removed old code

--- web/src/components/CompareCard.vue
+++ web/src/components/CompareCard.vue
@@ -30,7 +30,6 @@ import "prismjs/plugins/line-highlight/prism-line-highlight";
 import "prismjs/plugins/line-highlight/prism-line-highlight.css";
 import "prismjs/components/prism-python";
 import { BlockHighlightingOptions, registerBlockHighlighting } from "@/util/OccurenceHighlight";
-import { highlightLines, HighlightOptions } from "@/util/line-highlight/prism-line-highlight.ts";
 
 @Component
 export default class Compare extends Vue {
@@ -81,8 +80,6 @@ export default class Compare extends Vue {
           if (this.diff) {
             this.map = registerBlockHighlighting(this.diff, this.blockHighlightOptions);
             this.codeHighLight();
-            // this.blockHighlight();
-            // this.drawLineMarkers();
           }
           resolve();
         }).then(() => {
@@ -90,31 +87,6 @@ export default class Compare extends Vue {
         });
       }, 0);
     }
-
-    // drawLineMarkers(): void {
-    //   const codeLeft: HTMLElement = document.getElementById("codeLeft") as HTMLElement;
-    //   const codeRight: HTMLElement = document.getElementById("codeRight") as HTMLElement;
-    //
-    //   const options: HighlightOptions = {
-    //     classes: "line-marker",
-    //   };
-    //
-    //   for (let i = 1; i <= this.linesAmount("left"); i += 1) {
-    //     highlightLines(
-    //       codeLeft,
-    //       `${i}`,
-    //       { id: `line-left-${i}`, callback: event => this.lineClick(i, "left", event), ...options }
-    //     )();
-    //   }
-    //
-    //   for (let i = 1; i <= this.linesAmount("right"); i += 1) {
-    //     highlightLines(
-    //       codeRight,
-    //       `${i}`,
-    //       { id: `line-right-${i}`, callback: event => this.lineClick(i, "right", event), ...options }
-    //     )();
-    //   }
-    // }
 
     codeHighLight(): void {
       if (this.$refs.codeLeft) {
@@ -125,62 +97,6 @@ export default class Compare extends Vue {
         this.blockHighlightOptions.isLeftFile = false;
         Prism.highlightElement(this.$refs.codeRight as Element, false);
       }
-    }
-
-    blockHighlight(): void {
-      const codeLeft: HTMLElement = document.getElementById("codeLeft") as HTMLElement;
-      const codeRight: HTMLElement = document.getElementById("codeRight") as HTMLElement;
-
-      // this.leftLines = ;
-      // this.rightLines = new Array(this.linesAmount("right"));
-      const tempLeftLines: Array<Array<[number, string]>> = new Array(this.linesAmount("left"));
-      const tempRightLines: Array<Array<[number, string]>> = new Array(this.linesAmount("right"));
-
-      for (const index in this.diff.fragments) {
-        const block: Hunk = this.diff.fragments[index];
-        const baseID = `code-highlight-${index}`;
-
-        // register the fragments to the lines they span over
-        for (let i = block.left.startRow; i <= block.left.endRow; i += 1) {
-          if (!tempLeftLines[i]) {
-            tempLeftLines[i] = [];
-          }
-          tempLeftLines[i].push([block.right.startRow, baseID]);
-        }
-
-        for (let i = block.right.startRow; i <= block.right.endRow; i += 1) {
-          if (!tempRightLines[i]) {
-            tempRightLines[i] = [];
-          }
-          tempRightLines[i].push([block.left.startRow, baseID]);
-        }
-
-        const idLeft = `${baseID}-left`;
-        const idRight = `${baseID}-right`;
-
-        const options: HighlightOptions = {
-          classes: "code-highlight",
-          callback: this.scrollToCorrespondingBlock
-        };
-
-        highlightLines(
-          codeLeft,
-          `${block.left.startRow + 1}-${block.left.endRow + 1}`,
-          { id: idLeft, ...options }
-        )();
-
-        highlightLines(
-          codeRight,
-          `${block.right.startRow + 1}-${block.right.endRow + 1}`,
-          { id: idRight, ...options }
-        )();
-      }
-      function process(arr: Array<[number, string]>): Array<string> {
-        return arr.sort(([row1], [row2]) => row1 - row2).map(([, id]) => id);
-      }
-
-      this.leftLines = tempLeftLines.map(process);
-      this.rightLines = tempRightLines.map(process);
     }
 
     scrollToCorrespondingBlock(event: Event): void {
