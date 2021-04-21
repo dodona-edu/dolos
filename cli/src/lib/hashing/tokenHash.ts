616e13be8f03ff7f61a0f47a4f4fe239ae723bcf
Rien Maertens <rien.maertens@posteo.be>
2021-04-21 13:53:21 +0200

    Change the constants in RollingHash and TokenHash

--- cli/src/lib/hashing/tokenHash.ts
+++ cli/src/lib/hashing/tokenHash.ts
@@ -1,11 +1,37 @@
+/**
+ * Generates a hashing object that can be used to create hashes of a
+ * token string. It is based on the rolling hash used in the
+ * Rabin-Karp string matching algorithm, but without the rolling
+ * property.
+ *
+ * The hash base is chosen to be different than the one used in
+ * RollingHash as to not create collisions with similar subsequent
+ * tokens (e.g. hash(['abc'] should not equal hash(['a', 'b', 'c'])).
+ */
 export class TokenHash {
-  private readonly mod: number;
-  private readonly base: number;
+  /**
+   * The modulus used in the hash calculation.
+   *
+   * Since we want to be able to take the product of two hashes, the product of
+   * two hashes, the modulus multiplied by itself should not have more than the
+   * available amount of bits of precision.
+   *
+   * Javascript has 53-bit precision numbers (doubles) so. We pick the largest
+   * modulus m for which m * m has 53 bits.
+   */
+  readonly mod: number = 94906249;
 
-  constructor() {
-    this.mod = 33554467;
-    this.base = 5801;
-  }
+  /**
+   * The base (or radix) used in the hash calculation.
+   *
+   * We want that even small tokens use as much bits as possible. Because we
+   * assume the input token characters will mostly be less than 128, we pick
+   * our base such that multiplying with an input character use as much bits
+   * as possible.
+   *
+   * Hence this is the largest prime number such that 127 * base < mod.
+   */
+  readonly base: number = 747287;
 
   public hashToken(token: string): number {
     let hash = 0;
