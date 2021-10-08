d5f9663ec64f591d517c79c31622fb1e640f6f25
Arne <arne.carla.jacobs@outlook.com>
2021-10-08 21:46:38 +0200

    temp commit

---
+++ library/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: "ts-jest/presets/js-with-ts",
+  globals: {
+    "ts-jest": {
+      tsConfig: {
+        allowJs: true,
+      },
+    },
+  },
+};
---
+++ library/src/test.ts
@@ -0,0 +1,3 @@
+export function testFunction(something: string): void {
+  console.log("testtesttesttes" + something);
+}
