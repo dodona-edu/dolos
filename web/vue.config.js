789767ad13ebc3a20fb86071826febfb26dfe5aa
Arne <arne.carla.jacobs@outlook.com>
2021-10-12 12:33:21 +0200

    ignored error

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const webpack = require("webpack");
 
 module.exports = {
@@ -19,12 +20,12 @@ module.exports = {
   // explicitly makes sure that tree sitter is not loaded as it does not work in browser environments
   configureWebpack: {
     plugins: [
-      new webpack.IgnorePlugin({ 
-        resourceRegExp: /tree-sitter/ 
+      new webpack.IgnorePlugin({
+        resourceRegExp: /tree-sitter/
       }),
-      new webpack.IgnorePlugin({ 
-        resourceRegExp: /codeTokenizer/, 
-        contextRegExp: /library/ 
+      new webpack.IgnorePlugin({
+        resourceRegExp: /codeTokenizer/,
+        contextRegExp: /library/
       })
     ]
   }
