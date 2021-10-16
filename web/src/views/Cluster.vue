41fd695ed7fb13ff550cbcbe01db13e560d06cb9
AurisAudentis <maxiem@maxiemgeldhof.com>
2021-10-16 21:42:11 +0200

    changing default clustering value

--- web/src/views/Cluster.vue
+++ web/src/views/Cluster.vue
@@ -25,7 +25,7 @@ import { singleLinkageCluster } from "@/util/clustering-algorithms/SingleLinkage
   components: { ClusteringTable }
 })
 export default class Cluster extends DataView {
-  public cutoff = 0.25;
+  public cutoff = 0.5;
 
   created(): Promise<void> {
     return super.ensureData();
