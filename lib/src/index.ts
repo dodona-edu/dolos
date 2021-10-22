4385dfc2d9f152e04d19d933f5646e1ff1e00819
Arne <arne.carla.jacobs@outlook.com>
2021-10-22 11:48:52 +0200

    added global export file to lib

---
+++ lib/src/index.ts
@@ -0,0 +1,7 @@
+export { Report, ScoredPairs } from "./lib/analyze/report";
+export { Fragment } from "./lib/analyze/fragment";
+export { Region } from "./lib/util/region";
+export { Pair } from "./lib/analyze/pair";
+export { Options } from "./lib/util/options";
+export { Dolos } from "./dolos";
+
