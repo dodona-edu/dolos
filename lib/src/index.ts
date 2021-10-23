f5d49c208ccd88de352b0355ef498edb018ed573
Arne <arne.carla.jacobs@outlook.com>
2021-10-23 15:17:32 +0200

    added index file

---
+++ lib/src/index.ts
@@ -0,0 +1,7 @@
+export { WinnowingReport, ScoredPairs } from "./lib/analyze/winnowingReport";
+export { Fragment } from "./lib/analyze/fragment";
+export { Region } from "./lib/util/region";
+export { Pair } from "./lib/analyze/pair";
+export { Options } from "./lib/util/options";
+export { Dolos } from "./dolos";
+export { Report } from "./lib/analyze/report";
