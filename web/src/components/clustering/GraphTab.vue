e7d75a525b4b6505d8e857e9d0825691aaa41125
AurisAudentis <maxiem@maxiemgeldhof.com>
2021-10-25 17:07:19 +0200

    incorporating graph view in the cluster card

---
+++ web/src/components/clustering/GraphTab.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="graph-container">
+    <Graph
+      :pairs="clusterPairs"
+      :files="clusterFiles"
+      :cutoff="0"
+    ></Graph>
+  </div>
+</template>
+
+<script lang='ts'>
+import { Component, Prop, Watch } from "vue-property-decorator";
+import Graph from "../Graph.vue";
+import DataView from "@/views/DataView";
+import { Cluster } from "@/util/clustering-algorithms/ClusterTypes";
+import { getClusterElementsArray } from "@/util/clustering-algorithms/ClusterFunctions";
+import { Pair, File } from "@/api/api";
+
+@Component({
+  components: { Graph: Graph as any },
+})
+export default class GraphTab extends DataView {
+  @Prop() cluster!: Cluster;
+
+  clusterFiles: File[] = [];
+  clusterPairs: Pair[] = [];
+
+  mounted(): void {
+    this.ensureData();
+    this.updateClusterValues();
+  }
+
+  @Watch("cluster")
+  updateClusterValues(): void {
+    if (this.cluster) {
+      this.clusterFiles = getClusterElementsArray(this.cluster);
+      this.clusterPairs = Array.from(this.cluster);
+    } else {
+      this.clusterFiles = [];
+      this.clusterPairs = [];
+    }
+  }
+}
+</script>
+
+<style scoped>
+.graph-container {
+  min-height: 400px;
+}
+</style>
