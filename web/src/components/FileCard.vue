e977f02566ead337b3f105208163a2fdcad2b8b5
AurisAudentis <maxiem@maxiemgeldhof.com>
2021-11-06 17:02:24 +0100

    adding similarity histogram

--- web/src/components/FileCard.vue
+++ web/src/components/FileCard.vue
@@ -5,43 +5,8 @@
     </v-card-title>
     <div class="d-flex">
       <!-- Main part of the card -->
-      <div class="half-size">
-        <h5>Largest overlap:</h5>
-        <span>
-          The file
-          {{
-            getLargestOverlapPair(file)
-              .leftFile.path.split("/")
-              .slice(-2)
-              .join("/")
-          }}
-          has a total overlap of
-          <b>{{ getLargestOverlapPair(file).totalOverlap }}</b> tokens with this file.
-        </span>
-        <h5>Biggest similarity:</h5>
-        <span>
-          The file
-          {{
-            getLargestOverlapPair(file)
-              .leftFile.path.split("/")
-              .slice(-2)
-              .join("/")
-          }}
-          has a total similarity of
-          <b>{{ (getLargestOverlapPair(file).similarity * 100).toFixed(2) }}%</b> with this file.
-        </span>
-        <h5>Largest overlap:</h5>
-        <span>
-          The file
-          {{
-            getLargestOverlapPair(file)
-              .leftFile.path.split("/")
-              .slice(-2)
-              .join("/")
-          }}
-          has a largest common fragment of
-          <b>{{ getLargestOverlapPair(file).longestFragment }}</b> tokens with this file.
-        </span>
+      <div class="half-size" v-if="dataLoaded">
+        <FileSimilarityHistogram :file="file" />
       </div>
       <!-- Aside with extra info -->
       <div>
@@ -54,6 +19,36 @@
         </v-alert>
       </div>
     </div>
+    <h5>Largest overlap:</h5>
+    <span v-if="getLargestOverlapPair(file)">
+      The file
+      {{
+        getLargestOverlapPair(file).leftFile.path.split("/").slice(-2).join("/")
+      }}
+      has a total overlap of
+      <b>{{ getLargestOverlapPair(file).totalOverlap }}</b> tokens with this
+      file.
+    </span>
+    <h5>Biggest similarity:</h5>
+    <span v-if="getBiggestSimilarity(file)">
+      The file
+      {{
+        getBiggestSimilarity(file).leftFile.path.split("/").slice(-2).join("/")
+      }}
+      has a total similarity of
+      <b>{{ (getBiggestSimilarity(file).similarity * 100).toFixed(2) }}%</b>
+      with this file.
+    </span>
+    <h5>Largest overlap:</h5>
+    <span v-if="getLargestFragment(file)">
+      The file
+      {{
+        getLargestFragment(file).leftFile.path.split("/").slice(-2).join("/")
+      }}
+      has a largest common fragment of
+      <b>{{ getLargestFragment(file).longestFragment }}</b> tokens with this
+      file.
+    </span>
   </v-card>
 </template>
 
@@ -62,36 +57,55 @@ import { Component, Prop } from "vue-property-decorator";
 import { File, Pair } from "@/api/api";
 import { pairsAsNestedMapCached } from "@/util/PairAsNestedMap";
 import DataView from "@/views/DataView";
+import FileSimilarityHistogram from "./FileSimilarityHistogram.vue";
 
-@Component({})
+@Component({ components: { FileSimilarityHistogram } })
 export default class FileCard extends DataView {
   @Prop() file!: File;
   getTimestampText(file: File): string {
     return file.extra.timestamp?.toLocaleString() || "unknown";
   }
 
-  getLargestOverlapPair(file: File): Pair {
-    return Array.from(
-      pairsAsNestedMapCached(() => Object.values(this.pairs))
-        .get(file.id)!
-        .values()
-    ).reduce((p, p2) => (p.totalOverlap > p2.totalOverlap ? p : p2));
+  getLargestOverlapPair(file: File): Pair | null {
+    if (!this.dataLoaded) return null;
+
+    const pairMap = pairsAsNestedMapCached(() => Object.values(this.pairs)).get(
+      file.id
+    );
+
+    if (!pairMap) return null;
+
+    return Array.from(pairMap.values()).reduce((p, p2) =>
+      p.totalOverlap > p2.totalOverlap ? p : p2
+    );
   }
 
-  getBiggestSimilarity(file: File): Pair {
-    return Array.from(
-      pairsAsNestedMapCached(() => Object.values(this.pairs))
-        .get(file.id)!
-        .values()
-    ).reduce((p, p2) => (p.similarity > p2.similarity ? p : p2));
+  getBiggestSimilarity(file: File): Pair | null {
+    if (!this.dataLoaded) return null;
+
+    const pairMap = pairsAsNestedMapCached(() => Object.values(this.pairs)).get(
+      file.id
+    );
+
+    if (!pairMap) return null;
+
+    return Array.from(pairMap.values()).reduce((p, p2) =>
+      p.similarity > p2.similarity ? p : p2
+    );
   }
 
-  getLargestFragment(file: File): Pair {
-    return Array.from(
-      pairsAsNestedMapCached(() => Object.values(this.pairs))
-        .get(file.id)!
-        .values()
-    ).reduce((p, p2) => (p.longestFragment > p2.longestFragment ? p : p2));
+  getLargestFragment(file: File): Pair | null {
+    if (!this.dataLoaded) return null;
+
+    const pairMap = pairsAsNestedMapCached(() => Object.values(this.pairs)).get(
+      file.id
+    );
+
+    if (!pairMap) return null;
+
+    return Array.from(pairMap.values()).reduce((p, p2) =>
+      p.longestFragment > p2.longestFragment ? p : p2
+    );
   }
 }
 </script>
