de2f5958d6737c7e8c0adf349a1e1ece71550fca
AurisAudentis <maxiem@maxiemgeldhof.com>
2021-11-17 09:52:08 +0100

    add pagination, search, sort

--- web/src/views/Summary.vue
+++ web/src/views/Summary.vue
@@ -1,44 +1,19 @@
 <template>
   <div>
     <SummaryCard/>
-    <div class="d-flex align-center flex-column extra-margin">
-      <FileCard v-for="scoredFile in scoredFiles.slice(0,10)" :key="scoredFile.file.id" :file="scoredFile"/>
-    </div>
+    <SummaryList />
   </div>
 </template>
 <script lang="ts">
 import { Component } from "vue-property-decorator";
-import { FileScoring, FileInterestingnessCalculator } from "@/util/FileInterestingness";
 import DataView from "@/views/DataView";
 import SummaryCard from "@/components/summary/SummaryCard.vue";
-import FileCard from "@/components/summary/FileCard.vue";
+import SummaryList from "@/components/summary/SummaryList.vue";
 
 @Component({
-  components: { SummaryCard, FileCard }
+  components: { SummaryCard, SummaryList }
 })
 export default class Summary extends DataView {
-  private scoredFiles: FileScoring[] = [];
 
-  mounted(): void {
-    this.initializeData();
-  }
-
-  async initializeData(): Promise<void> {
-    await super.ensureData();
-    const scoringCalculator = new FileInterestingnessCalculator(Array.from(Object.values(this.pairs)));
-    const sortableFiles = Object.values(this.files).map(file => scoringCalculator.calculateFileScoring(file));
-    sortableFiles.sort((a, b) => b.finalScore - a.finalScore);
-    this.scoredFiles = sortableFiles;
-  }
 }
 </script>
-
-<style scoped>
-.extra-margin {
-  margin-top: 1.8rem;
-}
-
-.extra-margin * {
-  margin: 1rem;
-}
-</style>
