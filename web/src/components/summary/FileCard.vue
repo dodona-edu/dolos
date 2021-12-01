2516ab00a8e917a59aeabf74210fdf844d5eb403
AurisAudentis <maxiem@maxiemgeldhof.com>
2021-12-01 17:51:16 +0100

    adding new histogram

--- web/src/components/summary/FileCard.vue
+++ web/src/components/summary/FileCard.vue
@@ -6,7 +6,7 @@
     <div class="d-flex">
       <!-- Main part of the card -->
       <div class="half-size" v-if="dataLoaded">
-        <FileSimilarityHistogram :file="file.file" />
+        <SimilarityHistogram />
       </div>
       <!-- Aside with extra info -->
       <div>
@@ -32,14 +32,14 @@
 
 <script lang="ts">
 import { Component, Prop } from "vue-property-decorator";
-import { File, Pair } from "@/api/api";
+import { File } from "@/api/api";
 import DataView from "@/views/DataView";
 import FileSimilarityHistogram from "./FileSimilarityHistogram.vue";
 import FileCardScore from "./FileCardScore.vue";
-
+import SimilarityHistogram from "./SimilarityHistogram.vue";
 import { FileScoring } from "@/util/FileInterestingness";
 
-@Component({ components: { FileSimilarityHistogram, FileCardScore } })
+@Component({ components: { FileSimilarityHistogram, FileCardScore, SimilarityHistogram } })
 export default class FileCard extends DataView {
   @Prop() file!: FileScoring;
 
--- web/src/views/Summary.vue
+++ web/src/views/Summary.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <SummaryCard/>
+    <HistogramCard/>
     <SummaryList />
   </div>
 </template>
@@ -9,9 +10,10 @@ import { Component } from "vue-property-decorator";
 import DataView from "@/views/DataView";
 import SummaryCard from "@/components/summary/SummaryCard.vue";
 import SummaryList from "@/components/summary/SummaryList.vue";
+import HistogramCard from "@/components/summary/HistogramCard.vue";
 
 @Component({
-  components: { SummaryCard, SummaryList }
+  components: { SummaryCard, SummaryList, HistogramCard }
 })
 export default class Summary extends DataView {
 
