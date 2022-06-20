9dc1a6635ebad9406133f91780f062c575cee342
maarten.vanneyghem <maarten.vanneyghem@student.odisee.be>
2022-06-20 15:26:23 +0200

    Install Vue 2 composition api plugin

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,16 +1,22 @@
 import Vue from "vue";
+import VueCompositionAPI, { createApp } from "@vue/composition-api";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
 
-new Vue({
+Vue.use(VueCompositionAPI);
+
+const app = createApp({
   store,
   vuetify,
   router,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+});
+
+app.mount("#app");
