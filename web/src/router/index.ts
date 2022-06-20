7397e1a82988ed6b41b505e6f38858f355de2377
maarten.vanneyghem <maarten.vanneyghem@student.odisee.be>
2022-06-20 17:00:16 +0200

    Remove temporary test code

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -41,7 +41,7 @@ const routes: Array<RouteConfig> = [
     // route level code-splitting
     // this generates a separate chunk (compare.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "graph" */ "../views/Test.vue")
+    component: () => import(/* webpackChunkName: "graph" */ "../views/Overview.vue")
   },
 ];
 
