747d2e62f4435b55949b224b351f52516caf0f81
maarten.vanneyghem <maarten.vanneyghem@student.odisee.be>
2022-06-20 16:57:18 +0200

    Add pinia as store manager

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueCompositionAPI, { createApp } from "@vue/composition-api";
+import { PiniaVuePlugin, createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -11,12 +12,17 @@ import "@mdi/font/css/materialdesignicons.css";
 Vue.config.productionTip = false;
 
 Vue.use(VueCompositionAPI);
+Vue.use(PiniaVuePlugin);
 
+const pinia = createPinia();
 const app = createApp({
   store,
   vuetify,
   router,
-  render: h => h(App),
+  pinia,
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  render: (h) => h(App),
 });
 
 app.mount("#app");
