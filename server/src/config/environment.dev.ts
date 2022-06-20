5fb58a52bf3d59f934074ebeb9894bc77ceb7de4
Rien Maertens <rien.maertens@posteo.be>
2022-06-20 15:29:41 +0200

    Server: add missing host value to evironments

--- server/src/config/environment.dev.ts
+++ server/src/config/environment.dev.ts
@@ -2,5 +2,6 @@ import { Environment } from "./environment";
 
 export const DevEnvironment: Environment = {
   port: 3000,
-  baseURI: process.env.DOLOS_BASE_URL || "/test"
+  baseURI: process.env.DOLOS_BASE_URL || "/test",
+  host: 'localhost'
 };
