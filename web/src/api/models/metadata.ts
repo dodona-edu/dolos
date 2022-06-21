71b3af9def7736a30995b398b8d8044a065a3e6c
maarten.vanneyghem <maarten.vanneyghem@student.odisee.be>
2022-06-21 16:50:16 +0200

    Fix type errors

--- web/src/api/models/metadata.ts
+++ web/src/api/models/metadata.ts
@@ -2,7 +2,7 @@ export type MetaRowType =
   | { type: "string"; value: string }
   | { type: "boolean"; value: boolean }
   | { type: "number"; value: number }
-  | { type: "object"; value: null };
+  | { type: "object"; value: any };
 
 export interface Metadata {
   [k: string]: unknown;
