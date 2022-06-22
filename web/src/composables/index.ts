d25584b8d2098412d7717660a09db84b973c3818
maarten.vanneyghem <maarten.vanneyghem@student.odisee.be>
2022-06-22 11:50:45 +0200

    Create useRoute composable

--- web/src/composables/index.ts
+++ web/src/composables/index.ts
@@ -1,5 +1,6 @@
 export * from "./useVuetify";
 export * from "./useBreakpoints";
 export * from "./useRouter";
+export * from "./useRoute";
 export * from "./useLegend";
 export * from "./useClustering";
