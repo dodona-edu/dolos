0aa93ba67fb092e99c19fd1f26bd0f57650d3eee
maarten.vanneyghem <maarten.vanneyghem@student.odisee.be>
2022-06-22 17:21:31 +0200

    Refactor FileAnalysis component to Composition API

--- web/src/views/FileAnalysis.vue
+++ web/src/views/FileAnalysis.vue
@@ -5,15 +5,14 @@
   </div>
 </template>
 <script lang="ts">
-import { Component } from "vue-property-decorator";
-import DataView from "@/views/DataView";
+import { defineComponent } from "@vue/composition-api";
 import SummaryCard from "@/components/summary/SummaryCard.vue";
 import SummaryList from "@/components/summary/SummaryList.vue";
 
-@Component({
-  components: { SummaryCard, SummaryList }
-})
-export default class FileAnalysis extends DataView {
-
-}
+export default defineComponent({
+  components: {
+    SummaryCard,
+    SummaryList
+  }
+});
 </script>
