ce6d6d5d1aa4cfa2313dce5808dbb8e22302bee7
maarten.vanneyghem <maarten.vanneyghem@student.odisee.be>
2022-06-24 15:13:40 +0200

    Refactor SummaryCard component to Composition API

--- web/src/components/summary/SummaryCard.vue
+++ web/src/components/summary/SummaryCard.vue
@@ -1,68 +1,28 @@
 <template>
-  <div class="d-flex justify-center" :set="similarityPair = getHighestSimilarityPair()">
-    <v-card class="center-card" :set="highestOverlapPair = getHighestOverlapPair()">
+  <div class="d-flex justify-center">
+    <v-card class="center-card">
       <v-card-text class="center-card-element title">
         <h1>File analysis</h1>
         <div>
           <p class="text">
-            This page lists the most relevant pairs of files in your dataset. This is useful in particular when you
-            are looking for any plagiarism that may exist. </p><p class="text">
-
-            By default, a mix of different metrics is suggested. You can change the metrics of interest by selecting
-            a specific metric from the dropdown box.
-
-            </p><p class="text">
-          If you found a pair that looks interesting metric-wise, you can use the compare view to examine the files
-          in more detail.
+            This page lists the most relevant pairs of files in your dataset.
+            This is useful in particular when you are looking for any plagiarism
+            that may exist.
+          </p>
+          <p class="text">
+            By default, a mix of different metrics is suggested. You can change
+            the metrics of interest by selecting a specific metric from the
+            dropdown box.
+          </p>
+          <p class="text">
+            If you found a pair that looks interesting metric-wise, you can use
+            the compare view to examine the files in more detail.
           </p>
         </div>
       </v-card-text>
     </v-card>
   </div>
 </template>
-
-<script lang="ts">
-import { Component } from "vue-property-decorator";
-import { Pair } from "@/api/models";
-
-import DataView from "@/views/DataView";
-
-@Component({})
-export default class SummaryCard extends DataView {
-  mounted(): void {
-    super.ensureData();
-  }
-
-  getNumberOfFiles(): number {
-    return Object.keys(this.files || {}).length;
-  }
-
-  getHighestSimilarityPair(): Pair {
-    const pairs = Object.values(this.pairs || {});
-    return pairs.reduce(
-      (a, b) => ((a?.similarity || 0) > b.similarity ? a : b),
-      null
-    );
-  }
-
-  getHighestOverlapPair(): Pair {
-    const pairs = Object.values(this.pairs || {});
-
-    return pairs.reduce(
-      (a, b) => ((a?.longestFragment || 0) > b.longestFragment ? a : b),
-      null
-    );
-  }
-
-  similarityOfPair(p: Pair | null): number {
-    return p?.similarity || 0;
-  }
-
-  highestOverlapOfPair(p: Pair | null): number {
-    return p?.longestFragment || 0;
-  }
-}
-</script>
 
 <style scoped>
 .center-card {
@@ -73,6 +33,7 @@ export default class SummaryCard extends DataView {
   align-items: center;
   flex-direction: column;
   padding: 25px;
+  text-align: center;
 }
 
 .center-card-element {
@@ -88,7 +49,7 @@ export default class SummaryCard extends DataView {
 }
 
 .text {
-  color: rgba(0,0,0,0.8);
+  color: rgba(0, 0, 0, 0.8);
   font-size: 16px;
   line-height: 24px;
   font-family: "Roboto", sans-serif;
