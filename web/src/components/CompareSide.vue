129b73e98315f85ebce0d319cf4b7c3ad7ec7cbf
maarten.vanneyghem <maarten.vanneyghem@student.odisee.be>
2022-06-24 14:45:34 +0200

    Refactor CompareSide component to Composition API

--- web/src/components/CompareSide.vue
+++ web/src/components/CompareSide.vue
@@ -1,160 +1,226 @@
 <template>
   <div class="main-container">
     <component :is="'style'" type="text/css">
-
       <template v-for="item in activeSelections">
-        .token.marked-code.{{item}} {
-        background: var(--markedbg);
-        text-shadow: none;
-        }
+        .token.marked-code.{{ item }} { background: var(--markedbg);
+        text-shadow: none; }
       </template>
       <template v-for="item in hoveringSelections">
-        .token.marked-code.{{ item }} {
-          background: var(--hoveringbg);
-          text-shadow: none;
-        }
+        .token.marked-code.{{ item }} { background: var(--hoveringbg);
+        text-shadow: none; }
       </template>
       <template v-for="item in selectedSelections">
-        .token.marked-code.{{ item }} {
-          background: var(--selectedbg);
-          text-shadow: none;
-        }
+        .token.marked-code.{{ item }} { background: var(--selectedbg);
+        text-shadow: none; }
       </template>
     </component>
-    <pre v-scroll.self="onScroll"
-         ref="pre"
-         :id="identifier"
-         class="line-numbers highlighted-code"
-        :data-start="startRow"><code
+    <pre
+      v-scroll.self="onScroll"
+      ref="pre"
+      :id="identifier"
+      class="line-numbers highlighted-code"
+      :data-start="startRow"
+    ><code
       ref="codeblock" :class="`language-${language}`"></code>
     </pre>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue, Watch } from "vue-property-decorator";
+import {
+  defineComponent,
+  PropType,
+  computed,
+  watch,
+  onMounted,
+  onUnmounted,
+} from "@vue/composition-api";
 import { Selection, File } from "@/api/models";
+import {
+  ID_START,
+  registerFragmentHighlighting,
+} from "@/util/OccurenceHighlight";
+import { NodeStats } from "@dodona/dolos-lib";
 
 import Prism from "prismjs";
 import "prismjs/themes/prism.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.js";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
-import { ID_START, registerFragmentHighlighting } from "@/util/OccurenceHighlight";
-import { NodeStats } from "@dodona/dolos-lib";
 
-@Component
-export default class CompareSide extends Vue {
-  @Prop({ required: true }) identifier!: string;
-  @Prop({ required: true }) file!: File;
-  @Prop({ required: true }) language!: string;
-  @Prop({ required: true }) selections!: Array<Selection>;
-  @Prop({ required: true }) hoveringSelections!: Array<string>;
-  @Prop({ required: true }) activeSelections!: Array<string>;
-  @Prop({ required: true }) selectedSelections!: Array<string>;
-  @Prop({ required: true }) semanticMatches!: Array<NodeStats>;
-  @Prop({ default: 0 }) startRow!: number;
-  @Prop({ }) endRow?: number;
+export default defineComponent({
+  props: {
+    identifier: {
+      type: String as PropType<string>,
+      required: true,
+    },
 
-  get content(): string {
-    return this.file.content.split("\n").slice(this.startRow, this.endRow).join("\n");
-  }
+    file: {
+      type: Object as PropType<File>,
+      required: true,
+    },
 
-  onScroll(e: Event): void {
-    const scrollTop = (e.target as HTMLElement)?.scrollTop;
-    const maxScroll = (this.$refs.codeblock as HTMLElement).getBoundingClientRect().height;
-    const temp = (this.$refs.pre as HTMLElement).clientHeight;
-    this.$emit("codescroll", this.identifier, Math.min(1, scrollTop / (maxScroll - temp)));
-  }
+    language: {
+      type: String as PropType<string>,
+      required: true,
+    },
 
-  /**
-   * Returns an approximation of the amount of visible lines if all lines where filled in.
-   */
-  getLinesVisibleAmount(): number {
-    const lineNumber = document.querySelector(".line-numbers-rows :first-child");
-    if (!lineNumber) {
-      return 0;
-    }
-    const height = (this.$refs.pre as HTMLElement).getBoundingClientRect().height;
-    return height / lineNumber.getBoundingClientRect().height;
-  }
+    selections: {
+      type: Array as PropType<Selection[]>,
+      required: true,
+    },
 
-  async mounted(): Promise<void> {
-    // This timeout is needed to assure that the highlight function works. If this is not done then the first time
-    // component is loaded, the kmers will not be properly highlighted. This is probably due to the `Prism.hooks.add`
-    // call in OccurrenceHighlight#registerFragmentHighlighting happening too early.
-    setTimeout(async () => {
-      await this.highlight();
-    }, 50);
-    this.emitLinesVisibleAmount();
-    window.addEventListener("resize", this.emitLinesVisibleAmount);
-  }
+    hoveringSelections: {
+      type: Array as PropType<string[]>,
+      required: true,
+    },
 
-  destroyed(): void {
-    window.removeEventListener("resize", this.emitLinesVisibleAmount);
-  }
+    activeSelections: {
+      type: Array as PropType<string[]>,
+      required: true,
+    },
 
-  emitLinesVisibleAmount(): void {
-    this.$emit("linesvisibleamount", this.getLinesVisibleAmount());
-  }
+    selectedSelections: {
+      type: Array as PropType<string[]>,
+      required: true,
+    },
 
-  async installLanguage(): Promise<void> {
-    const currentLanguage: string = this.language.toLowerCase();
-    if (Prism.languages[currentLanguage]) {
-      return;
-    }
-    await require("prismjs/components/prism-" + currentLanguage);
-  }
+    semanticMatches: {
+      type: Array as PropType<NodeStats[]>,
+      required: true,
+    },
 
-  @Watch("file")
-  async highlight(): Promise<void> {
-    await this.installLanguage();
-    registerFragmentHighlighting(this.selections);
-    this.codeHighLight();
-  }
+    startRow: {
+      type: Number as PropType<number>,
+      default: 0,
+    },
 
-  addEventListeners(): void {
-    (this.$refs.pre as HTMLElement)
-      .addEventListener("click", () => this.$emit("selectionclick", this.identifier, []));
+    endRow: {
+      type: Number as PropType<number>,
+      default: undefined,
+    },
+  },
 
-    for (const value of document.querySelectorAll(`#${this.identifier} .marked-code`)) {
-      const filteredClassList = [...value.classList].filter(className => className.startsWith(ID_START));
-      value.addEventListener("click", (ev: Event) => {
-        this.$emit("selectionclick", this.identifier, filteredClassList);
-        ev.stopPropagation();
-      });
+  setup(props, { emit, refs }) {
+    const content = computed(() =>
+      props.file.content
+        .split("\n")
+        .slice(props.startRow, props.endRow)
+        .join("\n")
+    );
 
-      value.addEventListener("mouseout", () => {
-        this.$emit("selectionhoverexit", this.identifier, filteredClassList);
-      });
+    const onScroll = (e: Event): void => {
+      const scrollTop = (e.target as HTMLElement)?.scrollTop;
+      const maxScroll = (e.target as HTMLElement).getBoundingClientRect()
+        .height;
+      const temp = (e.target as HTMLElement).clientHeight;
+      emit(
+        "codescroll",
+        props.identifier,
+        Math.min(1, scrollTop / (maxScroll - temp))
+      );
+    };
 
-      value.addEventListener("mouseover", () => {
-        this.$emit("selectionhoverenter", this.identifier, filteredClassList);
-      });
-    }
-  }
+    // Returns an approximation of the amount of visible lines if all lines where filled in.
+    const getLinesVisibleAmount = (): number => {
+      const lineNumber = document.querySelector(
+        ".line-numbers-rows :first-child"
+      );
+      if (!lineNumber) {
+        return 0;
+      }
+      const height = (refs.pre as HTMLElement).getBoundingClientRect().height;
+      return height / lineNumber.getBoundingClientRect().height;
+    };
 
-  codeHighLight(): void {
-    const codeblock = this.$refs.codeblock as Element;
-    codeblock.textContent = this.content;
-    Prism.highlightElement(codeblock, false);
-    this.addEventListeners();
-  }
-}
+    const installLanguage = async (): void => {
+      const currentLanguage = props.language.toLowerCase();
+      if (Prism.languages[currentLanguage]) {
+        return;
+      }
+      await require("prismjs/components/prism-" + currentLanguage);
+    };
+
+    const addEventListeners = (): void => {
+      (refs.pre as HTMLElement)
+        .addEventListener("click", () => emit("selectionclick", props.identifier, []));
+
+      for (const value of document.querySelectorAll(`#${props.identifier} .marked-code`)) {
+        const filteredClassList = [...value.classList].filter(className => className.startsWith(ID_START));
+        value.addEventListener("click", (ev: Event) => {
+          emit("selectionclick", props.identifier, filteredClassList);
+          ev.stopPropagation();
+        });
+
+        value.addEventListener("mouseout", () => {
+          emit("selectionhoverexit", props.identifier, filteredClassList);
+        });
+
+        value.addEventListener("mouseover", () => {
+          emit("selectionhoverenter", props.identifier, filteredClassList);
+        });
+      }
+    };
+
+    const codeHighlight = (): void => {
+      const codeblock = refs.codeblock as Element;
+      codeblock.textContent = content.value;
+      Prism.highlightElement(codeblock, false);
+      addEventListeners();
+    };
+
+    const emitLinesVisibleAmount = (): void => {
+      emit("linesvisible", props.identifier, getLinesVisibleAmount());
+    };
+
+    const highlight = async (): void => {
+      await installLanguage();
+      registerFragmentHighlighting(props.selections);
+      codeHighlight();
+    };
+
+    watch(
+      () => props.file.content,
+      () => {
+        highlight();
+      }
+    );
+
+    onMounted(() => {
+      // This timeout is needed to assure that the highlight function works. If this is not done then the first time
+      // component is loaded, the kmers will not be properly highlighted. This is probably due to the `Prism.hooks.add`
+      // call in OccurrenceHighlight#registerFragmentHighlighting happening too early.
+      setTimeout(async () => {
+        await highlight();
+      }, 50);
+      emitLinesVisibleAmount();
+      window.addEventListener("resize", emitLinesVisibleAmount);
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener("resize", emitLinesVisibleAmount);
+    });
+
+    return {
+      content,
+      onScroll,
+    };
+  },
+});
 </script>
 
 <style lang="scss">
-@use 'variables';
+@use "variables";
 
 .highlighted-code {
-    min-height: 100%;
-    max-height: 100%;
-    overflow-y: scroll;
-    padding-top: 0 !important;
+  min-height: 100%;
+  max-height: 100%;
+  overflow-y: scroll;
+  padding-top: 0 !important;
 
-    .token {
-      margin: -4px 0 -4px 0;
-      padding: 4px 0 4px 0;
-    }
+  .token {
+    margin: -4px 0 -4px 0;
+    padding: 4px 0 4px 0;
+  }
 }
 
 pre.highlighted-code {
@@ -178,5 +244,4 @@ pre.highlighted-code {
 //pre::-webkit-scrollbar {
 //  width: 0; /* For Chrome, Safari, and Opera */
 //}
-
 </style>
