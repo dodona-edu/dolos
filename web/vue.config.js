39b9504e9bb71c14b7d27908af3964f77a203afe
maarten.vanneyghem <maarten.vanneyghem@student.odisee.be>
2022-06-28 13:47:31 +0200

    Cleanup vue config & remove unused dependencies

--- web/vue.config.js
+++ web/vue.config.js
@@ -4,29 +4,8 @@ const webpack = require("webpack");
 module.exports = {
   publicPath: "./",
   transpileDependencies: ["vuetify"],
-  chainWebpack: (config) => {
-    config.module
-      .rule("ts")
-      .use("ts-loader")
-      .tap((options) => {
-        options.compiler = "ttypescript";
-        return options;
-      });
-
-    config.module
-      .rule("tsx")
-      .use("ts-loader")
-      .tap((options) => {
-        options.compiler = "ttypescript";
-        return options;
-      });
-  },
   // explicitly makes sure that tree sitter is not loaded as it does not work in browser environments
   configureWebpack: {
-    devServer: {
-      host: "localhost",
-      headers: { "Access-Control-Allow-Origin": "*" },
-    },
     plugins: [
       new webpack.IgnorePlugin({
         resourceRegExp: /tree-sitter/,
