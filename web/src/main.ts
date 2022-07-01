7740b8c8526cc4ca381356a1928b5449a9233081
maarten.vanneyghem <maarten.vanneyghem@student.odisee.be>
2022-07-01 09:45:22 +0200

    Disable performance tracking

--- web/src/main.ts
+++ web/src/main.ts
@@ -9,7 +9,6 @@ import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
-Vue.config.performance = true;
 
 Vue.use(VueCompositionAPI);
 Vue.use(PiniaVuePlugin);
