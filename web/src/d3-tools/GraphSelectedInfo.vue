21fd5f8a76548145ca675d1e87f1e990f573d461
maarten.vanneyghem <maarten.vanneyghem@student.odisee.be>
2022-08-16 09:34:31 +0200

    Redesign selected node card

--- web/src/d3-tools/GraphSelectedInfo.vue
+++ web/src/d3-tools/GraphSelectedInfo.vue
@@ -3,17 +3,26 @@
     <transition name="scale-transition" mode="out-in">
       <div v-if="selectedNode">
         <v-card>
-          <v-card-title> Selected node </v-card-title>
+          <v-card-title>
+            Selected node
+          </v-card-title>
 
-          <v-card-text>
-            The author of this submission is
-            <b>{{ selectedNode.extra.fullName || "unknown" }}</b>
-            , belonging to group
-            <b>{{ selectedNode.extra.labels }}</b>
-            . The last hand-in date was
-            <b>{{ selectedNodeTimestamp || "not available." }}</b
-            >.
-          </v-card-text>
+          <v-list class="selected-info-list" dense>
+            <v-list-item class="selected-info-list-item">
+              <v-icon>mdi-account-outline</v-icon>
+              <span>{{ selectedNode.extra.fullName || "unknown" }}</span>
+            </v-list-item>
+
+            <v-list-item class="selected-info-list-item">
+              <v-icon>mdi-tag-outline</v-icon>
+              <span>{{ selectedNode.extra.labels || "unknown" }}</span>
+            </v-list-item>
+
+            <v-list-item class="selected-info-list-item">
+              <v-icon>mdi-clock-outline</v-icon>
+              <span>{{ selectedNodeTimestamp || "not available." }}</span>
+            </v-list-item>
+          </v-list>
         </v-card>
       </div>
     </transition>
@@ -49,85 +58,59 @@
   </div>
 </template>
 
-<script lang="ts">
-import {
-  defineComponent,
-  PropType,
-  computed,
-  toRef,
-} from "vue";
+<script lang="ts" setup>
+import { computed, toRef } from "vue";
 import { useCluster, useVuetify, useRouter } from "@/composables";
 import { File } from "@/api/models";
 import { Cluster, Clustering } from "@/util/clustering-algorithms/ClusterTypes";
 import { DateTime } from "luxon";
 import { getClusterElements } from "@/util/clustering-algorithms/ClusterFunctions";
 
-export default defineComponent({
-  props: {
-    currentClustering: {
-      type: Array as PropType<Clustering>,
-      required: true,
-    },
+interface Props {
+  currentClustering: Clustering;
+  selectedNode?: File;
+  selectedCluster?: Cluster;
+}
 
-    selectedNode: {
-      type: Object as PropType<File>,
-      required: false,
-    },
+const props = withDefaults(defineProps<Props>(), {});
 
-    selectedCluster: {
-      type: Set as PropType<Cluster | undefined>,
-      required: false,
-    },
-  },
+const vuetify = useVuetify();
+const router = useRouter();
+const { clusterFilesSet, clusterAverageSimilarity } = useCluster(toRef(props, "selectedCluster"));
 
-  setup(props) {
-    const vuetify = useVuetify();
-    const router = useRouter();
-    const { clusterFilesSet, clusterAverageSimilarity } =
-      useCluster(toRef(props, "selectedCluster"));
-
-    // Timestamp of the selected node.
-    const selectedNodeTimestamp = computed(() => {
-      if (props.selectedNode?.extra.timestamp) {
-        return DateTime.fromJSDate(
-          props.selectedNode.extra.timestamp
-        ).toLocaleString(DateTime.DATETIME_MED);
-      }
-      return null;
-    });
-
-    // index of the selected cluster.
-    const selectedClusterIndex = computed(() => {
-      if (!props.selectedCluster) return 0;
-
-      const sortFn = (a: Cluster, b:Cluster): number => getClusterElements(b).size - getClusterElements(a).size;
-      const sortedClustering = Array.from(props.currentClustering).sort(sortFn);
-      return sortedClustering.indexOf(props.selectedCluster);
-    });
-
-    // Go to the information section of this cluster.
-    const goToInfo = (): void => {
-      const clusterHash = `#clustering-card-${selectedClusterIndex.value}`;
-      // Navigate to the cluster card.
-      // The hash will be used by the clustering table to expand the correct cluster.
-      router.replace("#");
-      router.replace(clusterHash);
-
-      // Scroll to the cluster card.
-      vuetify.goTo(clusterHash);
-    };
-
-    return {
-      clusterFilesSet,
-      clusterAverageSimilarity,
-      selectedNodeTimestamp,
-      goToInfo,
-    };
-  },
+// Timestamp of the selected node.
+const selectedNodeTimestamp = computed(() => {
+  if (props.selectedNode?.extra.timestamp) {
+    return DateTime.fromJSDate(
+      props.selectedNode.extra.timestamp
+    ).toLocaleString(DateTime.DATETIME_MED);
+  }
+  return null;
 });
+
+// index of the selected cluster.
+const selectedClusterIndex = computed(() => {
+  if (!props.selectedCluster) return 0;
+
+  const sortFn = (a: Cluster, b:Cluster): number => getClusterElements(b).size - getClusterElements(a).size;
+  const sortedClustering = Array.from(props.currentClustering).sort(sortFn);
+  return sortedClustering.indexOf(props.selectedCluster);
+});
+
+// Go to the information section of this cluster.
+const goToInfo = (): void => {
+  const clusterHash = `#clustering-card-${selectedClusterIndex.value}`;
+  // Navigate to the cluster card.
+  // The hash will be used by the clustering table to expand the correct cluster.
+  router.replace("#");
+  router.replace(clusterHash);
+
+  // Scroll to the cluster card.
+  vuetify.goTo(clusterHash);
+};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .selected-info {
   max-width: 350px;
   position: absolute;
@@ -137,6 +120,16 @@ export default defineComponent({
   display: flex;
   flex-direction: column;
   gap: 1rem;
+
+  &-list {
+    padding-top: 0;
+
+    &-item {
+      display: flex;
+      gap: 0.5rem;
+      width: 100%;
+    }
+  }
 }
 
 .namecontainer {
