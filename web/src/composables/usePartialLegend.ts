58efcfa55ddfec3eb8fff3a50cf7420dae4d24c7
maarten.vanneyghem <maarten.vanneyghem@student.odisee.be>
2022-08-24 17:21:30 +0200

    Fix reactivity issues with partial legends

--- web/src/composables/usePartialLegend.ts
+++ web/src/composables/usePartialLegend.ts
@@ -1,5 +1,5 @@
 import { storeToRefs } from "pinia";
-import { ComputedRef, computed, unref } from "vue";
+import { WritableComputedRef, computed, unref } from "vue";
 import { MaybeRef } from "@/util/Types";
 import { File, Legend } from "@/api/models";
 import { useFileStore } from "@/api/stores";
@@ -7,23 +7,28 @@ import { useFileStore } from "@/api/stores";
 /**
  * Create a partial legend for the given files.
  */
-export function usePartialLegend(files: MaybeRef<File[]>): ComputedRef<Legend> {
+export function usePartialLegend(files: MaybeRef<File[]>): WritableComputedRef<Legend> {
   const fileStore = useFileStore();
   const { legend } = storeToRefs(fileStore);
 
-  return computed(() => {
-    const value = unref(files);
-    if (!value) return {};
+  return computed({
+    get() {
+      const value = unref(files);
+      if (!value) return {};
 
-    const partialLegend = { ...legend.value };
+      const partialLegend = { ...legend.value };
 
-    // Remove keys that do not have a label in the filesset.
-    for (const key of Object.keys(partialLegend)) {
-      if (!value.some(f => f.extra.labels === key)) {
-        delete partialLegend[key];
+      // Remove keys that do not have a label in the filesset.
+      for (const key of Object.keys(partialLegend)) {
+        if (!value.some(f => f.extra.labels === key)) {
+          delete partialLegend[key];
+        }
       }
-    }
 
-    return partialLegend;
+      return partialLegend;
+    },
+    set(value) {
+      legend.value = { ...legend.value, ...value };
+    }
   });
 }
