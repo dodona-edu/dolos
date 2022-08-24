75200293a215c59aa310f2526334ed3bcacfb6d5
maarten.vanneyghem <maarten.vanneyghem@student.odisee.be>
2022-08-24 16:10:22 +0200

    Add global settings modal

--- web/src/App.vue
+++ web/src/App.vue
@@ -11,6 +11,12 @@
           DOLOS
         </RouterLink>
       </v-toolbar-title>
+
+      <v-spacer />
+
+      <v-btn icon @click="settings = true">
+        <v-icon>mdi-cog</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-navigation-drawer
@@ -120,6 +126,60 @@
         <loading v-else :text="loadingText" />
       </v-container>
     </v-main>
+
+    <v-dialog
+      v-model="settings"
+      max-width="700"
+    >
+      <v-card>
+        <v-card-title>
+          Settings
+
+          <v-spacer />
+
+          <v-btn icon @click="settings = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+
+        <v-card-subtitle>
+          Configure global parameters of the analysis results.
+        </v-card-subtitle>
+
+        <v-card-text>
+          <div>
+            <h4>Similarity Threshold</h4>
+            <span class="text--secondary">
+              The similarity threshold is the minimum similarity a file pair must have to be considered plagiarised.
+            </span>
+          </div>
+          <similarity-setting compact />
+        </v-card-text>
+
+        <v-card-text class="d-flex justify-space-between align-center">
+          <div>
+            <h4>Anonymize Dataset</h4>
+            <span class="text--secondary">
+              Anonymize the dataset by removing the names of the authors and the files.
+            </span>
+          </div>
+
+          <v-switch
+            v-model="isAnonymous"
+            :disabled="!isLoaded"
+          />
+        </v-card-text>
+
+        <v-card-text>
+          <div>
+            <h4>Active labels</h4>
+            <span class="text--secondary">
+              Select the labels that should be displayed in the visualizations.
+            </span>
+          </div>
+        </v-card-text>
+      </v-card>  
+    </v-dialog>
   </v-app>
 </template>
 
@@ -130,6 +190,7 @@ import { useBreakpoints } from "@/composables";
 import { useApiStore } from "@/api/stores";
 import Loading from "@/components/Loading.vue";
 import packageJson from "../package.json";
+import SimilaritySetting from "./components/settings/SimilaritySetting.vue";
 
 const breakpoints = useBreakpoints();
 const api = useApiStore();
@@ -137,6 +198,8 @@ const { isLoaded, isAnonymous, loadingText } = storeToRefs(api);
 
 // If the drawer is open/closed.
 const drawer = shallowRef(breakpoints.value.desktop);
+// If the global settings dialog is open/closed.
+const settings = shallowRef(false);
 
 // Current version of the application.
 const version = computed(() => packageJson.version);
