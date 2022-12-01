0defd18fbdba97b7d5f5ac38689eb0e80aa91d30
Rien Maertens <rien.maertens@posteo.be>
2022-12-01 11:57:57 +0100

    Force SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,11 +39,11 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
