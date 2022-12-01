2c3f158362adc4fdccbabdf9016dbe443ba629f9
Rien Maertens <rien.maertens@posteo.be>
2022-12-01 11:51:36 +0100

    Annotate serializers

--- app/serializers/dataset_serializer.rb
+++ app/serializers/dataset_serializer.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: datasets
+#
+#  id                   :bigint           not null, primary key
+#  file_count           :integer
+#  name                 :string(255)
+#  programming_language :string(255)
+#  token                :string(255)      not null
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+# Indexes
+#
+#  index_datasets_on_token  (token)
+#
 class DatasetSerializer < ApplicationSerializer
   attributes :token, :programming_language, :zipfile
 
