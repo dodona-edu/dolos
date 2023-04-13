c3672d1b6792bb1f12a3f7391b72b86499b12cc6
maartenvn <maartenvn.eu@gmail.com>
2023-04-13 14:35:58 +0200

    Add back button to app bar

--- web/src/components/layout/Sidebar.vue
+++ web/src/components/layout/Sidebar.vue
@@ -7,68 +7,76 @@
     app
     left
   >
-    <v-list v-if="props.variant === 'analysis'" nav dense>
-      <v-list-item v-if="isServer" :to="{ name: 'Upload' }" link exact>
-        <v-list-item-icon>
-          <v-icon>mdi-paperclip</v-icon>
-        </v-list-item-icon>
-        <v-list-item-content>
-          <v-list-item-title>Back to upload</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+    <template v-if="props.variant === 'analysis'">
+      <v-list nav dense>
+        <v-list-item v-if="isServer" :to="{ name: 'Upload' }" link exact>
+          <v-list-item-icon>
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Back to upload</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
 
-      <v-list-item :to="{ name: 'Overview' }" link exact>
-        <v-list-item-icon>
-          <v-icon>mdi-chart-bar</v-icon>
-        </v-list-item-icon>
-        <v-list-item-content>
-          <v-list-item-title>Overview</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+      <v-divider />
 
-      <v-list-item :to="{ name: 'Submissions' }" link exact>
-        <v-list-item-icon>
-          <v-icon>mdi-file-document-multiple-outline</v-icon>
-        </v-list-item-icon>
+      <v-list nav dense>
+        <v-list-item :to="{ name: 'Overview' }" link exact>
+          <v-list-item-icon>
+            <v-icon>mdi-chart-bar</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Overview</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
 
-        <v-list-item-title>View by submission</v-list-item-title>
-      </v-list-item>
+        <v-list-item :to="{ name: 'Submissions' }" link exact>
+          <v-list-item-icon>
+            <v-icon>mdi-file-document-multiple-outline</v-icon>
+          </v-list-item-icon>
 
-      <v-list-item :to="{ name: 'Clusters' }" link exact>
-        <v-list-item-icon>
-          <v-icon>mdi-account-group-outline</v-icon>
-        </v-list-item-icon>
+          <v-list-item-title>View by submission</v-list-item-title>
+        </v-list-item>
 
-        <v-list-item-title>View by cluster</v-list-item-title>
-      </v-list-item>
+        <v-list-item :to="{ name: 'Clusters' }" link exact>
+          <v-list-item-icon>
+            <v-icon>mdi-account-group-outline</v-icon>
+          </v-list-item-icon>
 
-      <v-list-item :to="{ name: 'Graph' }" link exact>
-        <v-list-item-icon>
-          <v-icon>mdi-graph-outline</v-icon>
-        </v-list-item-icon>
+          <v-list-item-title>View by cluster</v-list-item-title>
+        </v-list-item>
 
-        <v-list-item-title>View by graph</v-list-item-title>
-      </v-list-item>
+        <v-list-item :to="{ name: 'Graph' }" link exact>
+          <v-list-item-icon>
+            <v-icon>mdi-graph-outline</v-icon>
+          </v-list-item-icon>
 
-      <v-list-item :to="{ name: 'Pairs' }" link exact>
-        <v-list-item-icon>
-          <v-icon>mdi-file-table-box-multiple-outline</v-icon>
-        </v-list-item-icon>
+          <v-list-item-title>View by graph</v-list-item-title>
+        </v-list-item>
 
-        <v-list-item-title>View by pair</v-list-item-title>
-      </v-list-item>
-    </v-list>
+        <v-list-item :to="{ name: 'Pairs' }" link exact>
+          <v-list-item-icon>
+            <v-icon>mdi-file-table-box-multiple-outline</v-icon>
+          </v-list-item-icon>
 
-    <v-list v-if="props.variant === 'upload'" nav dense>
-      <v-list-item :to="{ name: 'Upload' }" link>
-        <v-list-item-icon>
-          <v-icon>mdi-paperclip</v-icon>
-        </v-list-item-icon>
-        <v-list-item-content>
-          <v-list-item-title>Upload</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
+          <v-list-item-title>View by pair</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </template>
+
+    <template v-if="props.variant === 'upload'">
+      <v-list nav dense>
+        <v-list-item :to="{ name: 'Upload' }" link>
+          <v-list-item-icon>
+            <v-icon>mdi-paperclip</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Upload</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </template>
 
     <template #append>
       <v-divider />
