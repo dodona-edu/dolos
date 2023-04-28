f2fb04df5a088736d73e5fe9d087907367e9da16
maartenvn <maartenvn.eu@gmail.com>
2023-04-28 16:23:16 +0200

    Fix PageNavbar Vuetify 3 issues

--- web/src/components/layout/PageSidebar.vue
+++ web/src/components/layout/PageSidebar.vue
@@ -8,11 +8,11 @@
     left
   >
     <template v-if="props.variant === 'analysis'">
-      <v-list v-if="isServer" nav dense>
+      <v-list v-if="isServer" nav density="compact">
         <v-list-item  :to="{ name: 'Upload' }" link exact>
-          <v-list-item-icon>
+          <template #prepend>
             <v-icon>mdi-chevron-left</v-icon>
-          </v-list-item-icon>
+          </template>
           <v-list-item-content>
             <v-list-item-title>Back to upload</v-list-item-title>
           </v-list-item-content>
@@ -22,11 +22,11 @@
       <v-divider v-if="isServer" />
 
       <!-- Navigation when only one pair is available -->
-      <v-list v-if="pairs.hasOnlyOnePair" nav dense>
+      <v-list v-if="pairs.hasOnlyOnePair" nav density="compact">
         <v-list-item to="/" link>
-          <v-list-item-icon>
+          <template #prepend>
             <v-icon>mdi-chart-bar</v-icon>
-          </v-list-item-icon>
+          </template>
           <v-list-item-content>
             <v-list-item-title>Overview</v-list-item-title>
           </v-list-item-content>
@@ -34,44 +34,44 @@
       </v-list>
 
       <!-- Navifation when multiple pairs are available -->
-      <v-list v-if="!pairs.hasOnlyOnePair" nav dense>
+      <v-list v-if="!pairs.hasOnlyOnePair" nav density="compact">
         <v-list-item :to="{ name: 'Overview' }" link exact>
-          <v-list-item-icon>
+          <template #prepend>
             <v-icon>mdi-chart-bar</v-icon>
-          </v-list-item-icon>
+          </template>
           <v-list-item-content>
             <v-list-item-title>Overview</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
         <v-list-item :to="{ name: 'Submissions' }" link exact>
-          <v-list-item-icon>
+          <template #prepend>
             <v-icon>mdi-file-document-multiple-outline</v-icon>
-          </v-list-item-icon>
+          </template>
 
           <v-list-item-title>View by submission</v-list-item-title>
         </v-list-item>
 
         <v-list-item :to="{ name: 'Clusters' }" link exact>
-          <v-list-item-icon>
+          <template #prepend>
             <v-icon>mdi-account-group-outline</v-icon>
-          </v-list-item-icon>
+          </template>
 
           <v-list-item-title>View by cluster</v-list-item-title>
         </v-list-item>
 
         <v-list-item :to="{ name: 'Graph' }" link exact>
-          <v-list-item-icon>
+          <template #prepend>
             <v-icon>mdi-graph-outline</v-icon>
-          </v-list-item-icon>
+          </template>
 
           <v-list-item-title>View by graph</v-list-item-title>
         </v-list-item>
 
         <v-list-item :to="{ name: 'Pairs' }" link exact>
-          <v-list-item-icon>
+          <template #prepend>
             <v-icon>mdi-file-table-box-multiple-outline</v-icon>
-          </v-list-item-icon>
+          </template>
 
           <v-list-item-title>View by pair</v-list-item-title>
         </v-list-item>
@@ -79,11 +79,11 @@
     </template>
 
     <template v-if="props.variant === 'upload'">
-      <v-list nav dense>
+      <v-list nav density="compact">
         <v-list-item :to="{ name: 'Upload' }" link>
-          <v-list-item-icon>
+          <template #prepend>
             <v-icon>mdi-paperclip</v-icon>
-          </v-list-item-icon>
+          </template>
           <v-list-item-content>
             <v-list-item-title>Upload</v-list-item-title>
           </v-list-item-content>
@@ -94,44 +94,44 @@
     <template #append>
       <v-divider />
 
-      <v-list nav dense>
+      <v-list nav density="compact">
         <v-list-item
           href="https://github.com/dodona-edu/dolos"
           target="_blank"
           link
         >
-          <v-list-item-icon>
+          <template #prepend>
             <v-icon>mdi-github</v-icon>
-          </v-list-item-icon>
+          </template>
           <v-list-item-title>Github</v-list-item-title>
         </v-list-item>
 
         <v-list-item href="https://dolos.ugent.be" target="_blank" link>
-          <v-list-item-icon>
+          <template #prepend>
             <v-icon>mdi-help-circle-outline</v-icon>
-          </v-list-item-icon>
+          </template>
           <v-list-item-title>Documentation</v-list-item-title>
         </v-list-item>
 
         <v-list-item href="mailto:dodona@ugent.be" link>
-          <v-list-item-icon>
+          <template #prepend>
             <v-icon>mdi-email-outline</v-icon>
-          </v-list-item-icon>
+          </template>
           <v-list-item-title>Contact</v-list-item-title>
         </v-list-item>
       </v-list>
 
       <v-divider />
 
-      <v-list nav dense>
+      <v-list nav density="compact">
         <v-list-item
           href="https://github.com/dodona-edu/dolos/releases/"
           target="_blank"
           link
         >
-          <v-list-item-icon>
+          <template #prepend>
             <v-icon>mdi-tag-outline</v-icon>
-          </v-list-item-icon>
+          </template>
           <v-list-item-title> Dolos - v{{ version }} </v-list-item-title>
         </v-list-item>
       </v-list>
