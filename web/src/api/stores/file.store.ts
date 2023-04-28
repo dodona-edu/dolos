24a86cdec2c01924d95faab50b59ced754f47db9
maartenvn <maartenvn.eu@gmail.com>
2023-04-28 13:43:42 +0200

    Add proper error handling

--- web/src/api/stores/file.store.ts
+++ web/src/api/stores/file.store.ts
@@ -236,7 +236,7 @@ export const useFileStore = defineStore("file", () => {
 
   // Anonymize the data.
   function anonymize(): void {
-    apiStore.isLoaded = false;
+    apiStore.loading = false;
     apiStore.loadingText = "Anonymizing files...";
     const isAnonymous = apiStore.isAnonymous;
 
@@ -269,7 +269,7 @@ export const useFileStore = defineStore("file", () => {
       filesById.value = { ...filesById.value };
       pairStore.pairs = { ...pairStore.pairs };
       legend.value = { ...legend.value };
-      apiStore.isLoaded = true;
+      apiStore.loading = true;
     });
   }
 
