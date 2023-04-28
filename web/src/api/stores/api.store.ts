e654405ee968c08c6507c01e41b347a031cf1fbf
maartenvn <maartenvn.eu@gmail.com>
2023-04-28 13:43:42 +0200

    Add proper error handling

--- web/src/api/stores/api.store.ts
+++ web/src/api/stores/api.store.ts
@@ -21,9 +21,11 @@ export const useApiStore = defineStore("api", () => {
   const pairStore = usePairStore();
 
   // If the data is loaded.
-  const isLoaded = shallowRef(false);
+  const loading = shallowRef(true);
   // Loading text.
   const loadingText = shallowRef("Loading...");
+  // Error while loading.
+  const error = shallowRef();
 
   // Whether the names should be anonymized.
   const isAnonymous = shallowRef(false);
@@ -35,26 +37,30 @@ export const useApiStore = defineStore("api", () => {
 
   // Hydrate the API stores.
   const hydrate = async (): Promise<void> => {
-    isLoaded.value = false;
+    loading.value = true;
 
-    // Hydrate all stores (fetch data)
-    loadingText.value = "Fetching & parsing metadata...";
-    await metadataStore.hydrate();
+    try {
+      // Hydrate all stores (fetch data)
+      loadingText.value = "Fetching & parsing metadata...";
+      await metadataStore.hydrate();
 
-    loadingText.value = "Fetching & parsing files...";
-    await fileStore.hydrate();
-    loadingText.value = "Fetching & parsing k-grams...";
-    await kgramStore.hydrate();
+      loadingText.value = "Fetching & parsing files...";
+      await fileStore.hydrate();
+      loadingText.value = "Fetching & parsing k-grams...";
+      await kgramStore.hydrate();
 
-    loadingText.value = "Fetching & parsing pairs...";
-    await pairStore.hydrate();
+      loadingText.value = "Fetching & parsing pairs...";
+      await pairStore.hydrate();
 
-    // Calculate the initial cut-off value.
-    loadingText.value = "Calculating initial cut-off...";
-    cutoff.value = guessSimilarityThreshold(pairStore.pairsActiveList);
-    cutoffDefault.value = cutoff.value;
+      // Calculate the initial cut-off value.
+      loadingText.value = "Calculating initial cut-off...";
+      cutoff.value = guessSimilarityThreshold(pairStore.pairsActiveList);
+      cutoffDefault.value = cutoff.value;
+    } catch (e) {
+      error.value = e;
+    }
 
-    isLoaded.value = true;
+    loading.value = false;
   };
 
   const { dataUrl } = useAppMode();
@@ -67,8 +73,9 @@ export const useApiStore = defineStore("api", () => {
   return {
     url: dataUrl,
     isAnonymous,
-    isLoaded,
+    loading,
     loadingText,
+    error,
     cutoff,
     cutoffDefault,
     cutoffDebounced,
