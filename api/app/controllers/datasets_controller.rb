433ff2d274e122796f1a315507d30b2a6e3dcfc6
Rien Maertens <rien.maertens@posteo.be>
2024-03-15 14:06:18 +0100

    Remove unused dataset routes

--- api/app/controllers/datasets_controller.rb
+++ api/app/controllers/datasets_controller.rb
@@ -1,25 +1,9 @@
 class DatasetsController < ApplicationController
-  before_action :set_dataset, only: %i[show analyze destroy]
+  before_action :set_dataset, only: %i[show]
 
   # GET /datasets/1
   def show
     render json: @dataset
-  end
-
-  # POST /datasets/1/analyze
-  def analyze
-    @report = Report.new(dataset: @dataset)
-
-    if @report.save
-      render json: @report, status: :created, location: @report
-    else
-      render json: @report.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /datasets/1
-  def destroy
-    @dataset.destroy
   end
 
   private
@@ -27,10 +11,5 @@ class DatasetsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_dataset
     @dataset = Dataset.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def dataset_params
-    params.require(:dataset).permit(:zipfile, :name, :programming_language)
   end
 end
