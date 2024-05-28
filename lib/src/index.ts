e9e9ec08ab1b740f46028aa9ffa41f9c9ddd72d9
Rien Maertens <rien.maertens@posteo.be>
2024-05-28 15:14:31 +0200

    Export all from core and lib

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,5 +1,10 @@
-export { Dolos } from "./lib/dolos.js";
-export { Report } from "./lib/report.js";
-export { Options, CustomOptions } from "./lib/options.js";
-export { Language, LanguagePicker, LanguageError } from "./lib/language.js";
 export * from "@dodona/dolos-core";
+
+export * from "./lib/dolos.js";
+export * from "./lib/language.js";
+export * from "./lib/options.js";
+export * from "./lib/report.js";
+export * from "./lib/reader.js";
+export * from "./lib/tokenizer/charTokenizer.js";
+export * from "./lib/tokenizer/codeTokenizer.js";
+export * from "./lib/tokenizer/tokenizer.js";
